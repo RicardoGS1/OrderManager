e7fa33e88cac901819fe8eb1b8948ff01b95a268
RicardoGS1 <granadosacerioricardo@gmail.com>
2025-02-19 14:04:56 -0500

    add id

--- composeApp/src/commonMain/kotlin/com/virtualworld/mipymeanabelmaster/id/DataModule.kt
+++ composeApp/src/commonMain/kotlin/com/virtualworld/mipymeanabelmaster/id/DataModule.kt
@@ -1,13 +1,21 @@
 package com.virtualworld.mipymeanabelmaster.id
 
+import com.virtualworld.mipymeanabelmaster.core.data.FirestoreDataSourceImp
+import com.virtualworld.mipymeanabelmaster.core.repository.OrderRepository
+import com.virtualworld.mipymeanabelmaster.domain.usecase.GetOrderByCodeUseCase
 import com.virtualworld.mipymeanabelmaster.domain.usecase.GetOrdersSentUseCase
 import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
+import kotlin.math.sin
 
 val dataModules = module {
 
     factoryOf (::GetOrdersSentUseCase)
+    factoryOf (::GetOrderByCodeUseCase)
 
+    singleOf(::OrderRepository)
+    singleOf(::FirestoreDataSourceImp)
 
 }
 
