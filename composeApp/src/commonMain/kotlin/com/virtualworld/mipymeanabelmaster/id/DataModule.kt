268a9f8e0dd804da3b3088c42c1bd40ca4bdcfad
RicardoGS1 <granadosacerioricardo@gmail.com>
2025-03-05 11:44:39 -0500

    Configure Firebase initialization options and add UpdateOrderStateUseCase
    - Initialize Firebase with provided options in desktopMain.
    - inject `UpdateOrderStateUseCase`, and `NotificationSender` to `DataModule`.

--- composeApp/src/commonMain/kotlin/com/virtualworld/mipymeanabelmaster/id/DataModule.kt
+++ composeApp/src/commonMain/kotlin/com/virtualworld/mipymeanabelmaster/id/DataModule.kt
@@ -1,9 +1,11 @@
 package com.virtualworld.mipymeanabelmaster.id
 
+import com.virtualworld.mipymeanabelmaster.core.NotificationSender
 import com.virtualworld.mipymeanabelmaster.core.data.FirestoreDataSourceImp
 import com.virtualworld.mipymeanabelmaster.core.repository.OrderRepository
 import com.virtualworld.mipymeanabelmaster.domain.usecase.GetOrderByCodeUseCase
 import com.virtualworld.mipymeanabelmaster.domain.usecase.GetOrdersSentUseCase
+import com.virtualworld.mipymeanabelmaster.domain.usecase.UpdateOrderStateUseCase
 import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.singleOf
 import org.koin.dsl.module
@@ -13,9 +15,13 @@ val dataModules = module {
 
     factoryOf (::GetOrdersSentUseCase)
     factoryOf (::GetOrderByCodeUseCase)
+    factoryOf(::UpdateOrderStateUseCase)
 
     singleOf(::OrderRepository)
     singleOf(::FirestoreDataSourceImp)
+    singleOf(::NotificationSender)
+
+
 
 }
 
